4ed75cf9c9c3ebb2b1d002a24b40001e2633ddb2
DarkEye <lesko.matej.pu@gmail.com>
2023-09-09 22:17:52 +0200

    tweaking the build process

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vitejs.dev/config/
+// manifest is copied to 'dist' because of https://vitejs.dev/config/shared-options.html#publicdir
 export default defineConfig({
   plugins: [svelte()],
-})
+  assetsInclude: ["**/*.json"],
+  build: {
+    emptyOutDir: true,
+    watch: {},
+    lib: {
+      entry: {
+        popup: "index.html",
+      },
+      formats: ["es"],
+    },
+  },
+});
