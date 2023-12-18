591d78dd422b232889c13db66f4ca8ad3f82fa1d
DarkEye123 <lesko.matej.pu@gmail.com>
2023-12-18 14:25:43 +0100

    WIP

---
+++ src/Popup/TabViews/Default.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { Button, Switch, NativeSelect, TextInput } from "@svelteuidev/core";
+  import type { Option } from "../types";
+
+  export let supportedAppTargetVersions: Option[]
+  export let selectedAppVersion: string;
+</script>
+
+
+
+<section class="grid justify-items-center gap-2">
+    <NativeSelect
+      class="w-full"
+      data={supportedAppTargetVersions}
+      bind:value={selectedAppVersion}
+      on:change={handleOnVersionSelect}
+      placeholder="Pick one"
+      label="Select checkout version"
+    />
+  </section>
+  <h2 class="text-2xl font-bold">— OR —</h2>
+  <section>
+    <input
+      placeholder="Set PR version manually"
+      class="svelteUI-parody"
+      bind:this={customAppVersionInput}
+      type="number"
+      on:blur={handleOnCustomAppVersionInput}
+      on:keydown={(event) => {
+        if (event.key === "Enter") {
+          handleOnCustomAppVersionInput(event);
+        }
+      }}
+    />
+  </section>
--- src/Popup/index.svelte
+++ src/Popup/index.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
   import { AppName, type AppMessage } from "../types";
-  import type { Option } from "./types";
   import {
     readAllPullRequestsNumbers,
     sendSaveAppStateMessage,
@@ -8,10 +7,10 @@
   } from "./helpers";
   import { Button, Switch, NativeSelect, TextInput } from "@svelteuidev/core";
   import { Backpack, ArrowLeft } from "radix-icons-svelte";
+  import type { Option } from "./types";
 
   const Staging = "staging";
   const Production = "production";
-
   const WiredSupportedAppTargetVersions: Option[] = [
     {
       value: Staging,
@@ -24,6 +23,8 @@
   ];
 
   let supportedAppTargetVersions = [...WiredSupportedAppTargetVersions];
+
+  const tabs: "default" | "custom domain" = "default";
 
   let selectedAppVersion: string = supportedAppTargetVersions[0].value;
   let customAppVersion: number | null = null;
@@ -152,31 +153,7 @@
     </section>
     <h1>Ventrata Checkout Injector</h1>
     <div class="grid gap-6 rounded-lg border border-gray-200 p-4">
-      <section class="grid justify-items-center gap-2">
-        <NativeSelect
-          class="w-full"
-          data={supportedAppTargetVersions}
-          bind:value={selectedAppVersion}
-          on:change={handleOnVersionSelect}
-          placeholder="Pick one"
-          label="Select checkout version"
-        />
-      </section>
-      <h2 class="text-2xl font-bold">— OR —</h2>
-      <section>
-        <input
-          placeholder="Set PR version manually"
-          class="svelteUI-parody"
-          bind:this={customAppVersionInput}
-          type="number"
-          on:blur={handleOnCustomAppVersionInput}
-          on:keydown={(event) => {
-            if (event.key === "Enter") {
-              handleOnCustomAppVersionInput(event);
-            }
-          }}
-        />
-      </section>
+      
     </div>
     <footer class="grid gap-4">
       <Switch
